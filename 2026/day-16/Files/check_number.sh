e2dea344a8b77559fef08c9462e823b51e01232f
Naman Fakirde <fakirdr000@gmail.com>
2026-03-18 13:23:40 +0530

    Add script to check if a number is positive, negative, or zero

---
+++ 2026/day-16/Files/check_number.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+read -p "Enter a number: " num
+
+if [ $num -gt 0 ]; then
+        echo "Number is Positive"
+elif [ $num -lt 0 ]; then
+        echo "Number is Negative"
+else
+        echo "Number is Zero"
+fi
