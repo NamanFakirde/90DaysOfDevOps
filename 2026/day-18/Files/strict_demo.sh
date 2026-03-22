cb89ae1ea1747aba11da5be5e3192de313c68f0c
Naman Fakirde <fakirdr000@gmail.com>
2026-03-22 17:39:38 +0530

    Uncomment variable and command tests in strict_demo.sh
    Uncomment tests for undefined variable and failing command.

--- 2026/day-18/Files/strict_demo.sh
+++ 2026/day-18/Files/strict_demo.sh
@@ -4,13 +4,17 @@ set -euo pipefail
 
 echo "Script started"
 
-#echo "1) Testing undefined variable (set -u)"
-#echo "Value: $UNDEFINED_VAR"
+echo "1) Testing undefined variable (set -u)"
+echo "Value: $UNDEFINED_VAR"
 
-#echo "2) Testing failing command (set -e)"
-#ls /directory-that-does-not-exist
+echo "2) Testing failing command (set -e)"
+ls /directory-that-does-not-exist
 
 echo "3) Testing pipe failure (set -o pipefail)"
 echo "hello" | grep "world"
 
 echo "Script completed"
+
+
+# To test each mode -
+# Comment the rest two mode and then run 
